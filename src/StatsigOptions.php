839a1a945fddcb7e2efd62b29da9db6f40d686f7
Daniel Loomb <daniel@statsig.com>
2024-11-08 11:35:05 -0800

    chore: bump version to 0.0.1-beta.9

---
+++ src/StatsigOptions.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Statsig;
+
+class StatsigOptions
+{
+    public $__ref = null;
+
+    public function __construct(
+        $specs_url = null,
+        $log_event_url = null,
+        $specs_adapter = null,
+    ) {
+        $ffi = StatsigFFI::get();
+        $this->__ref = $ffi->statsig_options_create(
+            $specs_url,
+            $log_event_url,
+            is_null($specs_adapter) ? null : $specs_adapter->__ref
+        );
+    }
+
+    public function __destruct()
+    {
+        if (is_null($this->__ref)) {
+            return;
+        }
+
+        StatsigFFI::get()->statsig_options_release($this->__ref);
+        $this->__ref = null;
+    }
+}
